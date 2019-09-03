6febd96dc2ff9045b817d94ef2f09e608f4994a4
Zamariolo <vnsaugusto@gmail.com>
2019-09-03 09:13:09 -0300

    dTln e h adicionados

--- libSTC.py
+++ libSTC.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import math
 
 def tabela_fluidos(nome_fluido,t1,t2):
 	'''
@@ -13,6 +14,7 @@ def tabela_fluidos(nome_fluido,t1,t2):
 
 	Outputs: 
 			- list de dimensao (1,5) contendo rho, cp, kf, mi, pr [S.I.] nesta ordem.
+	Author: Vinicius e Andre
 	'''
 
 	print(nome_fluido)
@@ -56,6 +58,7 @@ def Nome_fluidos():
 	Outputs: 
 			- list com os nomes do fluidos de dimensao (1,n), onde n é o número de arquivos de 
 			  extensão ".csv" contidos na pasta database. 
+	Author: Vinicius e Andre
 	'''
 
 	files = os.listdir("database/fluidos")  # Carrega todos os arquivos contidos em database e salva em files
@@ -75,3 +78,76 @@ def Nome_fluidos():
 		Nomefluidos.append(nome[0:-4])
 	
 	return Nomefluidos     # Fim da função	
+
+def dtln(T1, T2, t1, t2):
+    '''
+	Descr: Calcula a variação de temperatura deltaTln
+
+	Inputs: 
+			- T1: Temperatura de entrada do fluido quente; [Qualquer unidade]
+            - T2: Temperatura de saida do fluido quente
+            - t1: Temperatura de entrada do fluido frio
+            - t2: Temperatura de saida do fluido frio
+
+	Outputs: 
+			- deltaTln
+	'''
+
+    if T1 > T2 and t1 < t2 :
+        dt1 = T1 - t2
+        dt2 = T2 - t1
+        if dt1 > 0 and dt2 > 0 :
+            if dt1 == dt2 :
+                dtln = dt1
+                return dtln
+            else :
+                dtln = (dt1-dt2)/np.log(dt1/dt2)
+                return dtln
+        else :
+            print ("Temperaturas inconsistentes!")
+    else :
+        print ("Temperaturas inconsistentes!")
+
+def Diam_Eq(w,e):
+	''' Descr: Calcula o diametro equivalente
+	Input: 
+			- w: 
+			- e: 
+	Outputs:
+			- De: diametro equivalente
+	Author: - Breno'''
+	De = 4*w*e/(2*(w+e))
+	return De
+
+def Area_ap(w,e,Np):
+	'''Descr: Calcula a area aparente
+	Input: 
+			- w: 
+			- e: 
+			- Np: Numero de placas
+	Outputs:
+			- De: Area aparente
+	Author: - Breno'''
+	a = w*e*((Np+1)/2)
+	return a
+
+def coef_convec(Pr,m,Deq,mi,ap,kf):   
+	''' Descr: Calcula os coeficientes de transferencia de calor do fluido frio e quente
+		   Obs: foi utilizada a correlação de Sinnot p/ o Nusselt
+	Inputs: 
+			- Pr: Numero de Prantl de ambos os fluidos em vetor [fluido 1, fluido 2];
+			- m: Vazão mássica [fluido 1, fluido 2];
+			- Deq: Diâmetro equivalente;
+			- mi: Viscosidade dinamica [fluido 1, fluido 2];
+			- ap: Area aparente
+			- kf: Condutividade térmica do fluido [fluido 1, fluido 2].
+
+	Outputs: 
+			- h: Coeficiente de transferencia de calor [fluido 1, fluido 2].
+	Author: Breno'''
+
+	h= np.zeros(2)
+	h[0] = 0.26 * math.pow(Pr[0],0.4)* math.pow((m[0]*Deq/(mi[0]*ap)),0.65) * (kf[0]/Deq)
+	h[1] = 0.26 * math.pow(Pr[1],0.4)* math.pow((m[1]*Deq/(mi[1]*ap)),0.65) * (kf[1]/Deq)
+	
+	return h
--- runTest.py
+++ runTest.py
@@ -19,3 +19,31 @@ print("Cp = %.2f " %resultados[1])
 print("Kf = %.2f " %resultados[2])
 print("Mi = %.2f " %resultados[3])
 print("Pr = %.2f " %resultados[4])
+
+print("Calculo de deltaTln")
+print("T1 = 200   T2 = 100")
+print("t1 = 50    t2 = 51")
+print("deltaTln=%.2f\n" %stc.dtln(200, 100, 50, 51))
+
+print("Diametro equivalente")
+print("w = 5   e = 8")
+print("Deq = %f\n" %stc.Diam_Eq(0.236,0.0007))
+
+print("Area aparente")
+print("w = 5 e = 8  Np=70")
+print("Ap = %f\n" %stc.Area_ap(0.236,0.0007,35))
+
+print("Pr=[2,268.2]")
+print("m = [1, 1.5]")
+print("mi=[2,1.308*10**(-3)]")
+print("kf=[1,0.02]\n")
+mi=[2,1.308*10**(-3)]
+kf=[1,0.02]
+Pr=[2,268.2]
+m = [1,1.5]
+Deq = stc.Diam_Eq(0.236,0.0007)
+ap = stc.Area_ap(0.236,0.0007,35)
+
+h = stc.coef_convec(Pr, m,Deq,mi,ap, kf)
+
+print("h = [%.2f, %.2f]" %(h[0], h[1]))
